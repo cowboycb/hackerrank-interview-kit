b1ed1792cee3a8a46b7245d94c816ed15d30f560
cihan biber <cihan.biber@signumtte.com>
2019-04-21 14:58:44 +0300

    sock_merchant ve counting_valleys implementations

---
+++ src/warmup_challenges/counting_valleys/Solution.java
@@ -0,0 +1,64 @@
+package warmup_challenges.counting_valleys;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+
+        Map<Character, Character> stepToggle = new HashMap<>();
+        stepToggle.put('U', 'D');
+        stepToggle.put('D', 'U');
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('U', 0);
+        map.put('D', 0);
+
+
+        Stack<Character> step = new Stack<>();
+
+        IntStream stream = s.chars();
+        stream.mapToObj(c -> (char)c).forEachOrdered(c -> {
+            char op = stepToggle.get(c);
+            if (!step.empty() && step.peek() == op){
+                step.pop();
+                if (step.empty()){
+                    map.put(op, map.get(op) + 1);
+                }
+            }else{
+                step.push(c);
+            }
+        });
+
+        return map.get('D');
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
+
---
+++ src/warmup_challenges/sock_merchant/Solution.java
@@ -0,0 +1,65 @@
+package warmup_challenges.sock_merchant;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        IntStream stream = Arrays.stream(ar);
+        AtomicInteger atomTotal = new AtomicInteger(0);
+        Map<Integer, Integer> mapSocks = new HashMap<>(ar.length);
+        int[] a = new int[1] ;
+        // Displaying elements in Stream
+        stream.forEach(s-> {
+            if (mapSocks.containsKey(s)){
+                atomTotal.getAndIncrement();
+                mapSocks.remove(s);
+                a[0]++;
+            }else{
+                mapSocks.put(s, 1);
+            }
+        });
+
+        return atomTotal.get();
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        System.out.println("Result " + result);
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
