168532269e1fda92a4d2165096e10ba983be8d29
cihan biber <bibercihan@gmail.com>
2019-04-23 09:20:09 +0300

    Jumping on the clouds implementation edit

--- src/warmup_challenges/jumping_on_the_clouds/Solution.java
+++ src/warmup_challenges/jumping_on_the_clouds/Solution.java
@@ -32,7 +32,6 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -50,10 +49,6 @@ public class Solution {
         int result = jumpingOnClouds(c);
 
         System.out.println(result);
-//        bufferedWriter.write(String.valueOf(result));
-//        bufferedWriter.newLine();
-//
-//        bufferedWriter.close();
 
         scanner.close();
     }
