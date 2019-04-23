32b06acd5abb1f59850659bd77d4426bad82eaf9
cihan biber <cihan.biber@signumtte.com>
2019-04-23 09:10:37 +0300

    Jumping on the clouds implementation

---
+++ src/warmup_challenges/jumping_on_the_clouds/Solution.java
@@ -0,0 +1,62 @@
+package warmup_challenges.jumping_on_the_clouds;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+
+        int totalJump = 0, idx;
+        for (idx = 0; idx < c.length; idx+=2) {
+            if (c[idx] == 1){
+                idx--;
+            }
+            totalJump ++;
+        }
+
+        if (idx != c.length+1){
+            return totalJump;
+        }
+        return totalJump-1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+
+        System.out.println(result);
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+
